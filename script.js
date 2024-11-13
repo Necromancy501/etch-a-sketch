8e1ed28119a383772e769289ed4ff5cb6dcc4568
Necromancy501 <94140246+Necromancy501@users.noreply.github.com>
2024-11-13 20:19:51 +0800

    Add "New Grid" button. Change inner structure
    Add new grid button that removes the previous grid and creates a new grid. If the user presses cancel
    it creates a default 16x16 grid.
    
    Also this code refactors the previous commit code by adding all of the behavior inside a createGrid function.
    This function gets called at the page start and whenever the user presses the button and enters a valid prompt.

--- script.js
+++ script.js
@@ -1,50 +1,78 @@
-const WIDTH = 16;
-const HEIGHT = 16;
+function createGrid(width, height){
 
-let numberOfBoxes = WIDTH;
-let numberOfRows = HEIGHT;
+    let numberOfBoxes = width;
+    let numberOfRows = height;
 
-const container = document.createElement("div");
+    const container = document.createElement("div");
 
-container.style.display = "flex";
-container.style.flexFlow = "column nowrap";
-container.style.width = "512px";
-container.style.alignItems = "center";
-container.style.margin = "auto";
-container.style.marginTop = "40px";
-container.style.border = "4px solid blue"; 
-container.style.borderRadius = "7px";
+    container.classList.add("container");
 
-while(numberOfRows){
-    const row = document.createElement("div");
-    row.style.display = "flex";
-    row.style.flexFlow = "row nowrap";
-    row.style.width = "512px";
-    while(numberOfBoxes){
-        const box = document.createElement("div");
-        box.classList.add("box");
-        box.style.padding = "0px";
-        box.style.margin = "0px";
-        box.style.border = "1px solid green";
-        box.style.display = "flex";
-        box.style.height = "30px";
-        box.style.flex = "0 0 30px";
-        row.appendChild(box);
-        numberOfBoxes--;     
+    container.style.display = "flex";
+    container.style.flexFlow = "column nowrap";
+    container.style.width = "512px";
+    container.style.alignItems = "center";
+    container.style.margin = "auto";
+    container.style.marginTop = "20px";
+    container.style.border = "4px solid blue"; 
+    container.style.borderRadius = "7px";
+
+    while(numberOfRows){
+        const row = document.createElement("div");
+        row.style.display = "flex";
+        row.style.flexFlow = "row nowrap";
+        row.style.width = "512px";
+        while(numberOfBoxes){
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.style.padding = "0px";
+            box.style.margin = "0px";
+            box.style.border = "1px solid green";
+            box.style.display = "flex";
+            box.style.height = "30px";
+            box.style.flex = "1 1 30px";
+            row.appendChild(box);
+            numberOfBoxes--;     
+        }
+        numberOfBoxes = width;
+        container.appendChild(row);
+        numberOfRows--;
     }
-    numberOfBoxes = WIDTH;
-    container.appendChild(row);
-    numberOfRows--;
-}
+    document.body.appendChild(container);
 
-document.body.appendChild(container);
+    const boxes = Array.from(document.querySelectorAll(".box"));
 
-const boxes = Array.from(document.querySelectorAll(".box"));
-
-boxes.forEach(function(elem) {
-    elem.addEventListener("mouseenter", function () {
-        elem.classList.add("paint");
-    });
+    boxes.forEach(function(elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.classList.add("paint");
+        });
 
 });
-console.log(boxes);
+}
+
+const button = document.createElement("button");
+button.textContent = ("New Grid");
+button.style.display = "flex";
+button.style.fontSize = "30px";
+button.style.padding = "10px";
+button.style.borderRadius = "12px";
+
+document.body.appendChild(button);
+
+createGrid(16, 16);
+
+const buttonOnPage = document.querySelector("button");
+
+buttonOnPage.addEventListener("click", () => {
+    const number = prompt("Please enter a number. This will be the width and height of the new grid (16 by default)");
+    const gridbox = document.querySelector(".container");
+    gridbox.remove();
+
+    if (number === null) {
+        createGrid(16,16);
+    }
+
+    else{
+    createGrid(number, number);
+    }
+});
+
